ba951a7580ede0a9329c46d2a8ce6656fd676003
KishoreVB70 <kishore07kzr@gmail.com>
2022-10-23 07:11:43 +0530

    made improvements

--- contracts/Dynamic.sol
+++ contracts/Dynamic.sol
@@ -4,9 +4,8 @@ pragma solidity >=0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Dynamic is ERC721URIStorage, Ownable {
+contract Dynamic is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private tokenIdGenerator;
     Counters.Counter public tokensUnsoldCount;
@@ -16,13 +15,17 @@ contract Dynamic is ERC721URIStorage, Ownable {
         uint256 tokenId;        
         uint256 price;
         address payable owner;
-        address payable seller;
-        bool sold;
+        bool forSale;
     }
 
     mapping(uint256 => Token) public tokens;
 
     constructor() ERC721("Dynamic", "DYN") {}
+
+    modifier onlyOwner(uint tokenId){
+        require(ownerOf(tokenId) == msg.sender, "Not owner");
+        _;
+    }
 
     // Function to mint a new token
     function mintToken(
@@ -35,22 +38,18 @@ contract Dynamic is ERC721URIStorage, Ownable {
 
         _safeMint(msg.sender, newId);
         _setTokenURI(newId, tokenURI);
-
-        sellToken(newId, price);
     }
 
     // Function to send a token for sale to the market place
     function sellToken(
         uint256 tokenId,
         uint256 price
-    ) public {
-        require(ownerOf(tokenId) == msg.sender, "Only owner can sell token");
+    ) public onlyOwner(tokenId){
         tokens[tokenId] = Token(
             tokenId,
             price,
-            payable(address(this)),
             payable(msg.sender),
-            false
+            true
         );
         tokensUnsoldCount.increment();
         _transfer(msg.sender, address(this), tokenId);
@@ -58,26 +57,34 @@ contract Dynamic is ERC721URIStorage, Ownable {
 
     // Function to purchase a token
     function buyToken(uint256 tokenId) public payable {
+        require(tokens[tokenId].forSale, "token Not For Sale");
+
         uint256 tokenCost = tokens[tokenId].price;
         require(
-            msg.sender != tokens[tokenId].seller,
-            "A seller cannot but thier own token"
+            msg.sender != tokens[tokenId].owner,
+            "Owner cannot but thier own token"
         );
         require(
             msg.value >= tokenCost,
             "Invalid amount of funds sent"
         );
 
-        address payable seller = tokens[tokenId].seller;
-        tokens[tokenId].sold = true;
+        address payable oldOwner = tokens[tokenId].owner;
+
+        tokens[tokenId].forSale = false;
         tokens[tokenId].owner = payable(msg.sender);
-        tokens[tokenId].seller = payable(address(0));
+
 
         tokensSoldCount.increment();
         _transfer(address(this), msg.sender, tokenId);
 
         // transfer token cost to token seller
-        seller.transfer(tokenCost);
+        oldOwner.transfer(tokenCost);
+    }
+
+    function cancelSale(uint tokenId) public onlyOwner(tokenId){
+        tokens[tokenId].forSale = false;
+        _transfer(address(this), msg.sender, tokenId);
     }
 
     // Function to return all tokens a user own
@@ -98,7 +105,7 @@ contract Dynamic is ERC721URIStorage, Ownable {
     // Function to check if a token still available for sale
     function tokenInMarket(uint256 tokenId) public view returns (bool) {
         require(_exists(tokenId), "Invalid token ID");
-        return !tokens[tokenId].sold;
+        return !tokens[tokenId].forSale;
     }
 
     // Function to return all tokens available for purchase
